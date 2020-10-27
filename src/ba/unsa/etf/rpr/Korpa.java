e216a19b9ede69412b64ad79b65a565a92b06355
mkadric5 <mkadric5@etf.unsa.ba>
2020-10-27 17:19:28 +0000

    Implementirane klase Artikl, Korpa i Supermarket, kao i Main klasa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli;
+
+    public Korpa() {
+        artikli = new Artikl[50];
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        for (int i=0; i<artikli.length;i++)
+            if (artikli[i]==null) {artikli[i]=a; return true;}
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i=0; i<artikli.length;i++)
+    if (artikli[i]!=null && artikli[i].getKod().equals(kod))
+    {
+    Artikl izbacena = artikli[i];
+    artikli[i]=null;
+    return izbacena;
+    }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int cijena=0;
+        for (Artikl a:artikli) if (a!=null)  cijena=cijena+a.getCijena();
+        return cijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli;
+
+    public Supermarket () {
+        artikli = new Artikl[1000];
+    }
+
+    public void dodajArtikl(Artikl a) {
+        for (int i=0;i<artikli.length;i++)
+            if (artikli[i]==null) {
+            artikli[i]=a; return;
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i=0; i<artikli.length;i++)
+        {
+            if (artikli[i]!=null && artikli[i].getKod().equals(kod))
+            {
+                Artikl izbacena = artikli[i];
+                artikli[i]=null;
+            return izbacena;}
+        }
+        return null;
+    }
 }
